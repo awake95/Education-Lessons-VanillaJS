3b7edd24939215309a3d503c7ee30f3c7971abfe
awake95 <42112469+awake95@users.noreply.github.com>
2020-01-23 15:55:10 +0300

    GloAcademy lesson1

---
+++ lesson01/src/main.js
@@ -0,0 +1,15 @@
+alert('Привет');
+
+let money = function () {
+};
+let income = function () {
+};
+let addExpenses = function () {
+};
+let deposit = function () {
+};
+let mission = function () {
+};
+let period = function () {
+};
+ console.log('money')
