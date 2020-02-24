7aefe7c882356f4f91e780658bdcbf025ccee1e7
awake95 <42112469+awake95@users.noreply.github.com>
2020-02-24 19:56:07 +0300

    Update main.js

--- lesson17/script/main.js
+++ lesson17/script/main.js
@@ -7,80 +7,54 @@ window.addEventListener('DOMContentLoaded', function () {
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
 
-        function getTimeRemaining() {
+        const getTimeRemaining = () => {
             let dateStop = new Date(deadline).getTime(),
                 dateNow = new Date().getTime(),
                 timeRemaining = ((dateStop - dateNow) / 1000),
                 seconds = Math.floor(timeRemaining % 60),
                 minutes = Math.floor((timeRemaining / 60) % 60),
-                hours = Math.floor(timeRemaining / 60 / 60) % 24;
+                hours = Math.floor(timeRemaining / 60 / 60);
             return {
                 timeRemaining,
                 hours,
                 minutes,
                 seconds
             };
-        }
+        };
 
+        const timeEdit = (timeElement) => {
+            if (timeElement < 10) {
+                return '0' + timeElement;
+            } else {
+                return timeElement;
+            }
+        };
+
+        let timerId;
         function updateClock() {
 
             let timer = getTimeRemaining();
 
-            timerHours.textContent = timer.hours;
-            timerMinutes.textContent = timer.minutes;
-            timerSeconds.textContent = timer.seconds;
-
-            if (timer.timeRemaining > 0) {
-
-                let idInterval = setInterval(updateClock, 1000);
-                setTimeout(() => {
-                    clearInterval(idInterval);
-                }, 10000);
-            }
-            
-        }
-
-
-        const timeEdit = (timeElement) => {
-            if (timeElement < 10) {
-                timeElement = '0' + timeElement;
-            }else {
-                return timeElement;
-        } }
-        
-        timeEdit();
-
-        function timeLine(){ 
             timerHours.textContent = timeEdit(timer.hours);
             timerMinutes.textContent = timeEdit(timer.minutes);
             timerSeconds.textContent = timeEdit(timer.seconds);
 
+            if (timer.timeRemaining <= 0) {
+                clearInterval(timerId);
+                timerHours.textContent = "00";
+                timerMinutes.textContent = "00";  
+                timerSeconds.textContent = "00";
+            }
+            
         }
-        timeLine();
 
-        // function updateClock() {
-        //     let timer = getTimeRemaining();
-
-        //     timerHours.textContent = timer.hours;
-        //     timerMinutes.textContent = timer.minutes;    
-        //     timerSeconds.textContent = timer.seconds;
-
-        //     if (timer.timeRemaining > 0) {
-
-        //         let idInterval = setInterval(updateClock, 1000);
-        //         count = 0;
-        //         clearInterval(idInterval);
-        //     }
-
-        // }
-
-
+        timerId = setInterval(updateClock, 1000);
+        console.log(timerId);
+        
         updateClock();
 
     }
 
 
-    countTimer('25 february 2019');
-
-
-});
+    countTimer('26 february 2020');
+})
