f32d61942f729d65d32fb856c17ae64ab1c87769
awake95 <42112469+awake95@users.noreply.github.com>
2020-03-04 00:25:24 +0300

    Update main.js

--- lesson21/script/main.js
+++ lesson21/script/main.js
@@ -296,17 +296,41 @@ window.addEventListener('DOMContentLoaded', function () {
     //team
 
     const team = () => {
-        
-        let command = document.querySelector('.command')
+        const command = document.querySelector('.command');
 
-        command.addEventListener('mouseover', (event) => {
-            let target = event.target;
-    
-            if (target.classList = 'command__photo') {
-                event.target.src = event.target.dataset.img;
-             }
-            });
+        const photo = event => {
+            const target = event.target;
+
+            if (target.classList.contains('command__photo')) {
+                let a = target.src,
+                    b = target.dataset.img;
+
+
+                target.dataset.img = a;
+                target.src = b;
+
+            }
         }
+
+        command.addEventListener('mouseover', photo)
+        command.addEventListener('mouseout', photo)
+
+    }
     team();
 
-})
+    //calc
+
+    const calc = () => {
+        const calcBlock = document.querySelector('.calc-block');
+
+        const calcItems = event => {
+            const target = event.target;
+            if (target.classList.contains('calc-item')) {
+                target.value = target.value.replace(/\D/g, '');
+            }
+        }
+        calcBlock.addEventListener('input', calcItems);
+    }
+    calc();
+
+});
