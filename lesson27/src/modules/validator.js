88aa55747c1aa0360a221e48cc6db338de466c22
alexwake95 <alexman9505@gmail.com>
2020-03-11 16:02:42 +0300

    r

---
+++ lesson27/src/modules/validator.js
@@ -0,0 +1,78 @@
+  //validator 
+
+  const validator = () => {
+    const checkInput = () => {
+        const allInput = document.querySelector('body'),
+            nameForm = document.getElementById('form2-name'),
+            value = nameForm.value;
+
+        const phone = event => {
+            const target = event.target;
+            if (target.classList.contains('form-phone')) {
+                target.value = target.value.replace(/[^0-9+]/g, '');
+            }
+        };
+
+        allInput.addEventListener('input', phone);
+
+        nameForm.addEventListener('input', (e) => {
+            let newValue = e.target.value;
+            if (newValue.match(/[^а-яА-Я]/g)) {
+                nameForm.value = value;
+                return;
+            }
+        });
+
+        const name = event => {
+            const target = event.target;
+            if (target.classList.contains('form-name')) {
+                target.value = target.value.replace(/[^а-яА-Я]/g, '');
+            }
+            if (target.classList.contains('mess')) {
+                target.value = target.value.replace(/[^а-яА-Я]/g, '');
+            }
+        }
+        allInput.addEventListener('input', name);
+
+    };
+
+    checkInput();
+
+    const form2 = document.getElementById('form2'),
+          form3 = document.getElementById('form3');
+
+    form2.addEventListener('submit', (event) => {
+        event.preventDefault();
+        form2.appendChild(statusMessage);
+        statusMessage.textContent = loadMessage;
+        const formData = new FormData(form2);
+       
+
+        formData.forEach((val, key) => {
+            body[key] = val;
+        });
+        bodyPost();
+        setTimeout(() => {
+            statusMessage.textContent = '';
+            form2.reset();
+          }, 5000);
+    });
+
+    form3.addEventListener('submit', (event) => {
+        event.preventDefault();
+        form3.appendChild(statusMessage);
+        statusMessage.textContent = loadMessage;
+        const formData = new FormData(form3);
+
+        formData.forEach((val, key) => {
+            body[key] = val;
+        });
+        bodyPost();
+        setTimeout(() => {
+            statusMessage.textContent = '';
+            form3.reset();
+          }, 5000);
+    });
+};
+
+export default validator;
